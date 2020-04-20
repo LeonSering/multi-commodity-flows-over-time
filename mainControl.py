9580c8dd6ae1c44730317f8d8e1e144e3c56c281
Max Zimmer <m.zimmer@campus.tu-berlin.de>
2020-04-20 13:43:56 +0200

    Initialization done

--- mainControl.py
+++ mainControl.py
@@ -8,7 +8,6 @@ from multiFlowClass import MultiFlow
 from argparse import ArgumentParser
 import pickle
 
-
 def load_graph(path):
     """
     Load graph instance from '.cg' file
@@ -27,6 +26,7 @@ def read_files(networkFile, inflowFile):
     :return: MultiFlow object
     """
     network = load_graph(networkFile)
+    # TODO: Check requirements here (e.g. is \tau_e > 0 f.a. e \in E?)
     mf = MultiFlow(network)
     with open(inflowFile, 'r') as fRead:
         firstLine = True
@@ -38,8 +38,7 @@ def read_files(networkFile, inflowFile):
                 rate, interval, path = line.split()
                 startTime, endTime = interval.split(",")
                 path = tuple(path.split(","))
-                mf.add_commodity(path, startTime, endTime, rate)
-
+                mf.add_commodity(path, float(startTime), float(endTime), float(rate))
     return mf
 
 
--- multiFlowClass.py
+++ multiFlowClass.py
@@ -5,7 +5,8 @@
 # Description:  Class managing the multi commodity flow
 # ===========================================================================
 
-from heapq import *
+import heapq
+from collections import OrderedDict
 
 class MultiFlow:
     """Manages the multi commodity flow"""
@@ -17,12 +18,30 @@ class MultiFlow:
         self.network = network.copy()
         self.pathCommodityDict = dict() # Format: path:(startTime, endTime, rate) //type(path) == tuple
         self.timeCommodityDict = dict() # Format: (startTime, endTime): (path, rate)
+        self.priority = None
+        self.inflow = {edge:OrderedDict() for edge in self.network.edges}  # f^+_e
+        self.outflow = {edge:OrderedDict() for edge in self.network.edges} # f^-_e
+        self.bIn = {edge:OrderedDict() for edge in self.network.edges} # b^+_e
+        self.bOut = {edge:OrderedDict() for edge in self.network.edges} # b^-_e
+        self.spillback = {node:OrderedDict() for node in self.network.nodes}   # c_v
 
     def add_commodity(self, path, startTime, endTime, rate):
         """Adds commodity to the dictionaries"""
         self.pathCommodityDict[path] = (startTime, endTime, rate)
         self.timeCommodityDict[(startTime, endTime)] = (path, rate)
 
+    def init_values(self, t):
+        """Init f, c, b up to initialTime t"""
+        minInf = -float('inf')
+        for v in self.network.nodes:
+            self.spillback[v][(minInf, t)] = 1
+        for e in self.network.edges:
+            v, w = e
+            tau = self.network[v][w]['transitTime']
+            self.inflow[e][(minInf, t)] = 0
+            self.outflow[e][(minInf, t)] = 0
+            self.bIn[e][(minInf, t + tau)] = self.network[v][w]['inCapacity'] # not full
+            self.bOut[e][(minInf, t + tau)] = 0
 
     def compute(self):
         """The priority heap maintained works as follows:
@@ -32,8 +51,15 @@ class MultiFlow:
             of all outgoing edges of v up to time t.
         """
 
-        # Get the minimal start time, this is the time up to which we know everything
+        # Get the minimal start time, this is the time up to which we know everything, as nothing happens before that
         initialTime = min(self.timeCommodityDict.keys())[0]
-        # Init priority heap
-        self.priority = heapify([(initialTime, node) for node in self.network.nodes])
+
+        # Init priority heap (No need to heapify, as this is sorted)
+        self.priority = [(initialTime, node) for node in self.network.nodes]
+
+        # Init f+, f-, c_v, b+_e, b-_e
+        self.init_values(initialTime)
+
+        # Access first element of heap
+        theta, v = heapq.heappop(self.priority)
 
